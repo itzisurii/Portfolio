7ae649f274263dc61aa49eefd793c15f441e0da8
itzisurii <itzIsurika08@gmail.com>
2025-08-09 16:10:47 +0530

    Done with JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
+const video = document.getElementById('bg-video');
+video.playbackRate = 0.75;  
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();  
+
+
+  const name = this.name.value;
+  const email = this.email.value;
+  const subject = this.subject.value;
+  const message = this.message.value;
+
+
+  console.log('Form Submission:');
+  console.log('Name:', name);
+  console.log('Email:', email);
+  console.log('Subject:', subject);
+  console.log('Message:', message);
+
+  alert("Your message has been sent!"); 
+
+  form.reset();
+});
 
